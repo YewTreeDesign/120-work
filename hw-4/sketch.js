e744e328c0469c5517970bdbb4c5f7d5f15e2f74
Charlene Mundel <35701097+YewTreeDesign@users.noreply.github.com>
2018-02-13 15:58:17 -0700

    test

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -1,24 +1,18 @@
-var bubbles=[];
-var logolable;
-
 function setup(){
-  createCanvas(600,800);
+createCanvas (800, 600);
+
 }
+
 function draw(){
-  background(0);
-  for (var i=bubbles.length-1; i >=0; i--){
-    bubbles[i].update();
-    bubbles[i] display;
-  }
+background(0);
+ellipse(364.257, 252.642, 26.712, 33.662);
+ellipse(444.898, 252.642, 26.712, 33.662);
+ellipse(347.63, 276.779, 22.533, 19.006);
+ellipse(461.934, 276.934, 22.533, 19.006);
+arc(405.708, 251.032, 156.007, 116.121 PI+QUARTER_PI, OPEN);
+()
 
-}
-function preload(){
-  for(var1=0;i<3;1++){
-    logolable[i]=loadImage('images/logolable'+1+'.png')
 
-}
-function mousePressed(){
-  var r= (random(0, logolable.length));
-  var b = new Bubble(mouseX, mouseY, logolable[r]);
-  bubbes.push(b);
+
+
 }
