f34ea4f13107c15295e745fdfac22015588f0532
Charlene Mundel <35701097+YewTreeDesign@users.noreply.github.com>
2018-02-28 14:12:51 -0700

    yeh

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -19,23 +19,11 @@ function setup() {
 
 function draw() {
   var pointillize = map(mouseX, 0, width, smallPoint, largePoint);
-  var x = floor(random(img.width));
-  var y = floor(random(img.height));
+  var x = floor(random(img.width)*1);
+  var y = floor(random(img.height)*1);
   var bx = ceil(map(mouseX, 0, 100, 0, 5));//pixles get smaller if you move it to the left
   var pix = img.get(x, y);
   fill(pix, 128);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
-  rect(x, y, pointillize, pointillize);
   rect(x, y, pointillize, pointillize);
 
 }
