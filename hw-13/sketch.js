e5fb2be7ce1ead926a408ef37d333564c5375035
Charlene Mundel <35701097+YewTreeDesign@users.noreply.github.com>
2018-05-03 03:21:27 -0600

    seals the game

--- hw-13/sketch.js
+++ hw-13/sketch.js
@@ -1,9 +1,12 @@
 let bubbles = [];
-
+let bg;
 let angerseal;
 let seal = [];
+let Score = 0;
 
 function preload() {
+  bg = loadImage('seals/bubble.jpg')
+
   angerseal = loadImage('seals/angerseal.png');
 
   for (let i = 0; i < 6; i++) {
@@ -13,6 +16,7 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   for (let i = 0; i < 1000; i++) {
     let x = 25 + 218 * i;
     let y = random(height);
@@ -27,12 +31,22 @@ function setup() {
 function mousePressed() {
   for (let i = 0; i < bubbles.length; i++) {
     bubbles[i].clicked(mouseX, mouseY);
+    let Score = 0;
+    Score = Score + 1;
+
   }
+    print("Score: " + Score);
 }
 
 
 function draw() {
-  background(0);
+  background(bg);
+  var s = "Score: ";
+  var x = 120;
+  var y = 220;
+  text(s + Score, x, y);
+  textSize(70);
+  fill( 255, 255, 255);
   for (let i = 0; i < bubbles.length; i++) {
     bubbles[i].move();
     bubbles[i].show();
@@ -55,12 +69,13 @@ class Bubble {
     //if (d < this.r) {
     if (px > this.x && px < this.x + this.r && py > this.y && py < this.y + this.r) {
       this.seal = angerseal; //random(seal);
-    }
+      Score = Score + 1;
+}
   }
 
   move() {
     this.x = this.x + random(-2, 1);
-    this.y = this.y - 3;
+    this.y = this.y - 5;
 
     if (this.y < 0) {
       this.y = height;
